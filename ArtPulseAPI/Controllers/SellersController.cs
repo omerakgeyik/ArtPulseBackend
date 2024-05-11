40d4656cbcc5019ba31e7aaf51d094c063b66895
Omer Akgeyik <omer.akgeyik@outlook.com>
2024-05-11 15:15:13 +0300

    seller controller was added and product controller was updated

---
+++ ArtPulseAPI/Controllers/SellersController.cs
@@ -0,0 +1,107 @@
+﻿using ArtPulseAPI.Data;
+using ArtPulseAPI.DTO;
+using ArtPulseAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtPulseAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SellersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public SellersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        //Adding Seller
+        [HttpPost("addSeller")]
+        public async Task<IActionResult> AddSeller(SellerDTO sellerDTO)
+        {
+            try
+            {
+                var seller = new Seller
+                {
+                    FirstName = sellerDTO.FirstName,
+                    LastName = sellerDTO.LastName,
+                    Email = sellerDTO.Email,
+                    PhoneNumber = sellerDTO.PhoneNumber,
+                    Address = sellerDTO.Address
+                };
+
+                _context.Sellers.Add(seller);
+                await _context.SaveChangesAsync();
+
+                return Ok("Seller added successfully.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        //Get all seller
+        [HttpGet("getAllSellers")]
+        public async Task<IActionResult> GetAllSellers()
+        {
+            try
+            {
+                var sellers = await _context.Sellers.ToListAsync();
+
+                if (sellers.Count == 0)
+                {
+                    return NotFound("No sellers found.");
+                }
+
+                var sellerDTOs = sellers.Select(s => SellerToDTO(s)).ToList();
+
+                return Ok(sellerDTOs);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        //Deleting Seller
+        [HttpDelete("deleteSeller/{id}")]
+        public async Task<IActionResult> DeleteSeller(int id)
+        {
+            try
+            {
+                var seller = await _context.Sellers.FindAsync(id);
+
+                if (seller == null)
+                {
+                    return NotFound("Seller not found.");
+                }
+
+                _context.Sellers.Remove(seller);
+                await _context.SaveChangesAsync();
+
+                return Ok("Seller deleted successfully.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        private SellerDTO SellerToDTO(Seller seller)
+        {
+            return new SellerDTO
+            {
+                Id = seller.Id,
+                FirstName = seller.FirstName,
+                LastName = seller.LastName,
+                Email = seller.Email,
+                PhoneNumber = seller.PhoneNumber,
+                Address = seller.Address
+            };
+        }
+    }
+
+}
--- ArtPulseAPI/Program.cs
+++ ArtPulseAPI/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(o => o.UseNpgsql("Host=localhost; Database=artpulse; Username=postgres; Password=12345"));
+builder.Services.AddDbContext<DataContext>(o => o.UseNpgsql("Host=localhost; Database=artpulse; Username=postgres; Password=1234"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
